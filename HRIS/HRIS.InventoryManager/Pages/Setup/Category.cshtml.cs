5389cf2174d1602a74a5441b943bab30b1d3bb3c
aikemefuna <aikemefuna3@gmail.com>
2021-06-08 11:00:04 +0100

    Crerated Category Page

---
+++ HRIS/HRIS.InventoryManager/Pages/Setup/Category.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HRIS.InventoryManager.Pages.Setup
+{
+    public class CategoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
